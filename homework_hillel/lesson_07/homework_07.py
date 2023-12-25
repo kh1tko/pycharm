ec4aac0a7f7ca766c09d5b7128c054e2874485b6
Artem <hitko21@gmail.com>
2023-12-25 12:11:42 +0200

    little fix hw 7

--- homework_hillel/lesson_07/homework_07.py
+++ homework_hillel/lesson_07/homework_07.py
@@ -1,12 +1,25 @@
+# 1) сортування списка по зростанню числа, від меншого до більшого.
+# Функція приймає список з чисел і повертає відсортований список.
+
 def list_sorted_up(list_a: list):
     return sorted(list_a)
 
+
+# 2) сортування списка на спад, від  більшого до меншого.
+# Функція приймає список з чисел і повертає відсортований список.
 
 def list_sorted_down(list_a: list):
     return sorted(list_a, reverse=True)
 
 
+# 3) сортування за кількістю букв у слові.
+# Функція приймає список з слів і повертає відсортований список.
+
 def list_sorted_words(list_a: list):
     return sorted(list_a, key=len)
 
 
+if __name__ == '__main__':
+    print(list_sorted_up([5, 4, -6, 9, 0, 5, 4, 98, 12]))
+    print(list_sorted_down([9, 65, 74, 101, 13, 7, 86, 65]))
+    print(list_sorted_words(['BMW', 'Renault', 'Mercedes', 'Suzuki', 'Volvo', 'Tesla']))
--- homework_hillel/lesson_07/test_homework_07.py
+++ homework_hillel/lesson_07/test_homework_07.py
@@ -1,21 +1,16 @@
-from pythonProject.homework_hillel.lesson_07.homework_07 import list_sorted_up
-from pythonProject.homework_hillel.lesson_07.homework_07 import list_sorted_down
-from pythonProject.homework_hillel.lesson_07.homework_07 import list_sorted_words
+from homework_07 import list_sorted_up
+from homework_07 import list_sorted_down
+from homework_07 import list_sorted_words
 
 
 def test_list_sorted_up():
-    input_list = [5, 4, -6, 9, 0, 5, 4, 98, 12]
-    expected_result = sorted(input_list)
-    assert list_sorted_up(input_list) == expected_result
+    assert list_sorted_up([5, 4, -6, 9, 0, 5, 4, 98, 12]) == [-6, 0, 4, 4, 5, 5, 9, 12, 98]
 
 
 def test_list_sorted_down():
-    input_list = [9, 65, 74, 101, 13, 7, 86, 65]
-    expected_result = sorted(input_list, reverse=True)
-    assert list_sorted_down(input_list) == expected_result
+    assert list_sorted_down([9, 65, 74, 101, 13, 7, 86, 65]) == [101, 86, 74, 65, 65, 13, 9, 7]
 
 
 def test_list_sorted_words():
-    input_list = ['BMW', 'Renault', 'Mercedes', 'Suzuki', 'Volvo', 'Tesla']
-    expected_result = sorted(input_list, key=len)
-    assert list_sorted_words(input_list) == expected_result
+    assert (list_sorted_words(['BMW', 'Renault', 'Mercedes', 'Suzuki', 'Volvo', 'Tesla'])
+            == ['BMW', 'Volvo', 'Tesla', 'Suzuki', 'Renault', 'Mercedes'])
