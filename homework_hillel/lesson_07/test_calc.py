849848edbed1173ca6c21cf30b2e558ddede1d38
Artem <hitko21@gmail.com>
2023-12-25 18:01:28 +0200

    add to hw7

---
+++ homework_hillel/lesson_07/test_calc.py
@@ -0,0 +1,15 @@
+from lesson_07 import calc_dict
+from lesson_07 import concatenate
+
+
+# pytest
+def test_calc_addition():
+    assert calc_dict["+"](3, 7) == 10, "Повинно бути 10 "
+
+
+def test_calc_subs():
+    assert calc_dict["-"](13, 7) == 6
+
+
+def test_concatenate():
+    assert concatenate("a", "b") == "ab"
