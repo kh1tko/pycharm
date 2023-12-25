f72c000151b8a9fb8d828661599c76f74c2cfa69
Artem <hitko21@gmail.com>
2023-12-25 18:01:16 +0200

    add to hw7

---
+++ homework_hillel/lesson_07/test_postman.py
@@ -0,0 +1,8 @@
+import requests
+
+
+def test_status_code():
+    url = "https://postman-rest-api-learner.glitch.me//info"
+    payload = {"name": "Add your name in the body"}
+    response = requests.request("POST", url, json=payload)
+    assert response.status_code == 200
