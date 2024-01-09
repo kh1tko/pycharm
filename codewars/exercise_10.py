e95a0726d4956fbe300c162baecc9d44dea7f7be
Artem <hitko21@gmail.com>
2024-01-09 16:05:57 +0200

    fix

--- codewars/exercise_10.py
+++ codewars/exercise_10.py
@@ -6,5 +6,10 @@ def no_space(x):
     return x.replace(' ', '')
 
 
+def find_smallest_int(arr):
+    return min(arr)
+
+
 if __name__ == '__main__':
     print(no_space('ggg ggg ttt ttt'))
+    print(find_smallest_int([1, 2, 4.5, 50.5, 0, -9]))
