d631cbf4673645c00a89821589dcf1428daca696
Artem <hitko21@gmail.com>
2024-01-10 19:04:45 +0200

    tasks

--- codewars/exercise_10.py
+++ codewars/exercise_10.py
@@ -1,3 +1,6 @@
+from itertools import count
+
+
 def solution(text, ending):
     return text.endswith(ending)
 
@@ -10,6 +13,23 @@ def find_smallest_int(arr):
     return min(arr)
 
 
+def count_sheeps(sheep):
+    return sheep.count(True)
+
+
+def basic_op(operator, value1, value2):
+    if operator == '-':
+        return value1 - value2
+    elif operator == '+':
+        return value1 + value2
+    if operator == '*':
+        return value1 * value2
+    if operator == '/':
+        return value1 // value2
+
+def century(year):
+
+
+
 if __name__ == '__main__':
-    print(no_space('ggg ggg ttt ttt'))
-    print(find_smallest_int([1, 2, 4.5, 50.5, 0, -9]))
+    print(basic_op('-', 9, 8))
