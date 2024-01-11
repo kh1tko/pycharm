449b4d38f017eddb181ee2a52f76a4a7bac0ebb4
Artem <hitko21@gmail.com>
2024-01-11 20:43:50 +0200

    little fix

--- codewars/exercise_10.py
+++ codewars/exercise_10.py
@@ -27,9 +27,35 @@ def basic_op(operator, value1, value2):
     if operator == '/':
         return value1 // value2
 
-def century(year):
 
+def abbrev_name(name):
+    result = name.split()
+    new_name = '.'.join(result[0][0] + result[1][0])
+    return new_name.upper()
+
+
+def digitize(n):
+    numbers = [int(i) for i in str(n)]
+    reversed_digits = list(reversed(numbers))
+    return reversed_digits
+
+
+def compare_versions(version1, version2):
+    components1 = list(map(int, version1.split('.')))
+    components2 = list(map(int, version2.split('.')))
+
+    max_len = max(len(components1), len(components2))
+    components1 += [0] * (max_len - len(components1))
+    components2 += [0] * (max_len - len(components2))
+
+    for comp1, comp2 in zip(components1, components2):
+        if comp1 > comp2:
+            return True
+        elif comp1 < comp2:
+            return False
+
+    return True
 
 
 if __name__ == '__main__':
-    print(basic_op('-', 9, 8))
+    print(compare_versions('10.4.6', '10.5'))
