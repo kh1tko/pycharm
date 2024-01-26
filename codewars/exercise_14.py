3f1f5bea6e0728a435b0857bfa08ba76f57befc2
Artem <hitko21@gmail.com>
2024-01-26 15:53:03 +0200

    extra tasks for my exercise

--- codewars/exercise_14.py
+++ codewars/exercise_14.py
@@ -5,5 +5,25 @@ def remove_every_other(my_list):
     return new_list
 
 
+def converter(mpg):
+    LITERS_PER_GALLON = 4.54609188
+    KM_PER_MILE = 1.609344
+    kpl = mpg * KM_PER_MILE / LITERS_PER_GALLON
+    return round(kpl, 2)
+
+
+def get_sum(a, b):
+    if a == b:
+        return a
+    elif a < b:
+        count = list(range(a, b + 1))
+        return sum(count)
+    else:
+        count = list(range(b, a - 1))
+        return sum(count)
+
+
 if __name__ == '__main__':
-    print(remove_every_other(['Hello', 'Goodbye', 'Hello Again']))
+    # print(remove_every_other(['Hello', 'Goodbye', 'Hello Again']))
+    # print(converter(10))
+    print(get_sum(-870, -76))
