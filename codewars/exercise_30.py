5fdef63d3928bae85d8b5a05d297e842144071f9
Artem <hitko21@gmail.com>
2024-03-16 14:25:19 +0200

    another example of exercise

--- codewars/exercise_30.py
+++ codewars/exercise_30.py
@@ -3,6 +3,30 @@ def remove_smallest(numbers: list):
     return numbers
 
 
+def add(a, b):
+    return a + b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
+
+
+def mod(a, b):
+    return a % b
+
+
+def exponent(a, b):
+    return a ** b
+
+
+def subt(a, b):
+    return a - b
+
+
 if __name__ == "__main__":
     print(remove_smallest([1, 2, 3, 4, 5]))
     print(remove_smallest([274, 80, 360, 194, 146, 228, 360]))
