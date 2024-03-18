a746ba2ef3f5808a3a403c0c91e38f01f1ab87c2
Artem <hitko21@gmail.com>
2024-03-18 21:36:37 +0200

    new day, new activity

--- codewars/exercise_28.py
+++ codewars/exercise_28.py
@@ -4,5 +4,13 @@ def reverse_words(s):
     return ' '.join(x_)
 
 
+def boolean_to_string(b):
+    if b == True:
+        return 'True'
+    else:
+        return 'False'
+
+
 if __name__ == '__main__':
     print(reverse_words('The greatest victory is that which requires no battle'))
+    print(boolean_to_string(True))
