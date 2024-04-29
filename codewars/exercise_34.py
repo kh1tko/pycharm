7e904c68018905b4bdb0c43f65dd12c8ac399a52
Artem <hitko21@gmail.com>
2024-04-29 21:58:16 +0300

    another example of exercise

--- codewars/exercise_34.py
+++ codewars/exercise_34.py
@@ -1,2 +1,8 @@
-def find_average(numbers: list):
-    return sum(numbers) / len(numbers)
+def foo(positional, only, /, either, pos, or_='default',
+        *, keyword, just, **keywords):
+    print(positional, only, either, pos, or_, keyword,
+          just, keywords)
+
+
+foo('a', 'b', 1, 2, 'or_', keyword='keyword', just='just',
+    unknown_keyword='unknown_keyword')
